793a31d5442f8c1f17a56f9a76aeba544630c6af
CrockerUC <crockecf@mail.uc.edu>
2026-03-16 12:40:58 -0400

    Part C finished

--- src/DevTest.java
+++ src/DevTest.java
@@ -7,8 +7,10 @@ public class DevTest
         Scanner scanner = new Scanner(System.in);
 //        String userString = getNonZeroLenString(scanner, "Enter a string");
 //        System.out.println("You entered: " + userString);
-        int userInt = getInt(scanner, "Enter an integer");
-        System.out.println("You entered: " + userInt);
+//        int userInt = getInt(scanner, "Enter an integer");
+//        System.out.println("You entered: " + userInt);
+//        double userDouble = getDouble(scanner, "Enter a double");
+//        System.out.println("You entered: " + userDouble);
     }
 
     /**
@@ -38,28 +40,56 @@ public class DevTest
      * @param prompt prompt for the user
      * @return an integer response
      */
-    public static int getInt(Scanner pipe, String prompt)
-    {
-        boolean done = false;
-        int val = 0;
-        do
-        {
-            System.out.print(prompt + ": ");
-            if(pipe.hasNextInt())
-            {
-                val = pipe.nextInt();
-                pipe.nextLine();
-                done = true;
-            }
-            else
-            {
-                String trash = pipe.nextLine();
-                System.out.println("Enter a valid integer, not " + trash);
-            }
-        } while(!done);
-        return val;
-    }
+//    public static int getInt(Scanner pipe, String prompt)
+//    {
+//        boolean done = false;
+//        int val = 0;
+//        do
+//        {
+//            System.out.print(prompt + ": ");
+//            if(pipe.hasNextInt())
+//            {
+//                val = pipe.nextInt();
+//                pipe.nextLine();
+//                done = true;
+//            }
+//            else
+//            {
+//                String trash = pipe.nextLine();
+//                System.out.println("Enter a valid integer, not " + trash);
+//            }
+//        } while(!done);
+//        return val;
+//    }
 
 
+    /**
+     * A method that prompts the user to input any double. (That is that we don't have any pre-conception
+     * of what the range for the double might be.)
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a double response
+     */
+//    public static double getDouble(Scanner pipe, String prompt)
+//    {
+//        boolean done = false;
+//        double val = 0.0;
+//        do
+//        {
+//            System.out.print(prompt + ": ");
+//            if(pipe.hasNextDouble())
+//            {
+//                val = pipe.nextDouble();
+//                pipe.nextLine();
+//                done = true;
+//            }
+//            else
+//            {
+//                String trash = pipe.nextLine();
+//                System.out.println("Enter a valid double, not " + trash);
+//            }
+//        } while(!done);
+//        return val;
+//    }
 
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -11,14 +11,72 @@ public class SafeInput
      */
     public static String getNonZeroLenString(Scanner pipe, String prompt)
     {
-        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        String retString; // Set this to zero length. Loop runs until it isn't
         do
         {
             System.out.print("\n" +prompt + ": "); // show prompt add space
             retString = pipe.nextLine();
-        }while(retString.length() == 0);
+        }while(retString.isEmpty());
 
         return retString;
 
+    }
+
+    /**
+     * A method that prompts the user to input any integer. (That is that we don't have any pre-conception
+     * of what the range for the integer might be.)
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return an integer response
+     */
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        boolean done = false;
+        int val = 0;
+        do
+        {
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextInt())
+            {
+                val = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                String trash = pipe.nextLine();
+                System.out.println("Enter a valid integer, not " + trash);
+            }
+        } while(!done);
+        return val;
+    }
+
+    /**
+     * A method that prompts the user to input any double. (That is that we don't have any pre-conception
+     * of what the range for the double might be.)
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a double response
+     */
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        boolean done = false;
+        double val = 0.0;
+        do
+        {
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextDouble())
+            {
+                val = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                String trash = pipe.nextLine();
+                System.out.println("Enter a valid double, not " + trash);
+            }
+        } while(!done);
+        return val;
     }
 }
