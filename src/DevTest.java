34133d98315db5c0a34b936e76834ac1cb1e95b5
CrockerUC <crockecf@mail.uc.edu>
2026-03-16 12:25:11 -0400

    Part A finished

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class DevTest
 {
     public static void main(String[] args)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        String userString = getNonZeroLenString(scanner, "Enter a string");
+        System.out.println("You entered: " + userString);
     }
+
+    /**
+     * A method that gets a non-zero-length String from the user.
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
+
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class SafeInput
 {
-    
+    /**
+     * A method that gets a non-zero-length String from the user.
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
 }
