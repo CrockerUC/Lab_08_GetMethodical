413efbd1ae3b66d9b99d85f03631715c33f9d167
CrockerUC <crockecf@mail.uc.edu>
2026-03-16 12:34:35 -0400

    Part B finished

--- src/DevTest.java
+++ src/DevTest.java
@@ -5,8 +5,10 @@ public class DevTest
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        String userString = getNonZeroLenString(scanner, "Enter a string");
-        System.out.println("You entered: " + userString);
+//        String userString = getNonZeroLenString(scanner, "Enter a string");
+//        System.out.println("You entered: " + userString);
+        int userInt = getInt(scanner, "Enter an integer");
+        System.out.println("You entered: " + userInt);
     }
 
     /**
@@ -16,17 +18,48 @@ public class DevTest
      * @param prompt prompt for the user
      * @return a String response that is not zero length
      */
-    public static String getNonZeroLenString(Scanner pipe, String prompt)
+//    public static String getNonZeroLenString(Scanner pipe, String prompt)
+//    {
+//        String retString = ""; // Set this to zero length. Loop runs until it isn't
+//        do
+//        {
+//            System.out.print("\n" +prompt + ": "); // show prompt add space
+//            retString = pipe.nextLine();
+//        }while(retString.length() == 0);
+//
+//        return retString;
+//
+//    }
+
+    /**
+     * A method that prompts the user to input any integer. (That is that we don't have any pre-conception
+     * of what the range for the integer might be.)
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return an integer response
+     */
+    public static int getInt(Scanner pipe, String prompt)
     {
-        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        boolean done = false;
+        int val = 0;
         do
         {
-            System.out.print("\n" +prompt + ": "); // show prompt add space
-            retString = pipe.nextLine();
-        }while(retString.length() == 0);
-
-        return retString;
-
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextInt())
+            {
+                val = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                String trash = pipe.nextLine();
+                System.out.println("Enter a valid integer, not " + trash);
+            }
+        } while(!done);
+        return val;
     }
+
+
 
 }
